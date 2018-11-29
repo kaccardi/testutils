c074e6758eca9febf6cc7e73409d92753c3d705e
Kristen Carlson Accardi <kristen@linux.intel.com>
2018-11-29 12:54:02 -0800

    semaphoreci: initial commit
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>

---
+++ semaphoreci/install_dependencies.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function updateUbuntu {
+    sudo apt-get -y update
+    sudo apt-get install -y git flex build-essential fakeroot libncurses5-dev libssl-dev ccache libelf-dev bc python3-pip qemu-kvm qemu libburn4 libisoburn1 libisofs6 libjte1 xorriso systemd-services --fix-missing
+}
+
+function setupGoLang {
+    pushd /tmp
+    wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+    sudo tar -xvf go1.11.linux-amd64.tar.gz
+    sudo mv go /usr/local/
+    popd
+    export GOROOT=/usr/local/go
+    export PATH=/usr/local/go/bin:$PATH:/bin:/usr/bin
+    export PATH=$PATH:$(go env GOPATH)/bin
+    export GOPATH=$HOME
+}
+
+function setupCcloudVM {
+    setupGoLang
+    mkdir -p ~/.ccloudvm/workloads
+    cp testutils/ccloudvm/kernel.yaml ~/.ccloudvm/workloads
+    pushd $(go env GOPATH)/src/github.com/intel
+    mv ccloudvm ccloudvm.orig
+    ln -s ../kaccardi/ccloudvm ccloudvm
+    cd ccloudvm
+    git checkout origin/disable-kvm
+    go install ./...
+    popd
+}
+
+function createTestVM {
+    ccloudvm setup
+    ccloudvm create kernel
+    ccloudvm status
+    status=`ccloudvm status | grep VM | cut -d : -f 2`
+    echo "status is $status"
+    while [[ $status == "VM down" ]]; do
+        sleep 5
+        status=`ccloudvm status | grep VM | cut -d : -f 2`
+        echo "status is $status"
+    done
+}
+
+updateUbuntu
+setupCcloudVM
+createTestVM
+myssh=`ccloudvm status | grep ssh`
+mysshcmd=`echo $myssh | cut -d : -f 2`
+$mysshcmd "mount"
+
