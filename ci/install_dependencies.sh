2305f91b0792d957b38071b47f8c3f3465840437
Kristen Carlson Accardi <kristen@linux.intel.com>
2018-12-13 09:56:54 -0800

    debug
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -6,14 +6,6 @@ source semaphoreci.sh
 updateUbuntu
 setupCcloudVM
 createTestVM
-ccloudvm status
-myssh=`ccloudvm status | grep ssh`
-mysshcmd=`echo $myssh | cut -d : -f 2 | sed -e 's/^[ \t]*//'`
-echo "ssh string is $mysshcmd"
-retval=`$mysshcmd "ls /home"`
-echo "return value $?"
-echo "retval is $retval"
-retval=`$mysshcmd "mount"`
-echo "return value $?"
-echo "retval is $retval"
-
+sshcmd=$(getSSH)
+echo $sshcmd
+$sshcmd "ls -l"
