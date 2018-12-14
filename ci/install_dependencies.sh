9bc51363d5269e2edda63bf74f314490ffbcdb27
Kristen Carlson Accardi <kristen@linux.intel.com>
2018-12-14 13:04:25 -0800

    generate ssh keys for the test vm
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -5,6 +5,9 @@ DIR="${BASH_SOURCE%/*}"
 source "$DIR/ccloudvm.sh"
 source "$DIR/semaphoreci.sh"
 
+# create some keys for the test VM
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
 updateUbuntu
 setupCcloudVM
 createTestVM
