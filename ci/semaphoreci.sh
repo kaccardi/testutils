ee301679c53ae651138aac6344b49811a1889c25
Kristen Carlson Accardi <kristen@linux.intel.com>
2019-01-03 13:17:18 -0800

    Add an initial test config for the fedora28 test vm.
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>

--- ci/semaphoreci.sh
+++ ci/semaphoreci.sh
@@ -5,7 +5,7 @@
 
 function updateUbuntu {
     sudo apt-get -y update
-    sudo apt-get install -y git flex bison build-essential fakeroot libncurses5-dev libssl-dev ccache libelf-dev bc python3-pip qemu-kvm qemu libburn4 libisoburn1 libisofs6 libjte1 xorriso systemd-services --fix-missing
+    sudo apt-get install -y git flex bison build-essential fakeroot libncurses5-dev libssl-dev ccache libelf-dev bc python3-pip qemu-kvm qemu libburn4 libisoburn1 libisofs6 libjte1 xorriso systemd-services rpm --fix-missing
     sudo apt-get autoremove -y
     sudo apt-get clean -y
 }
