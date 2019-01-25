298026066f644095cf6046d2dec265e656ecffc3
Kristen Carlson Accardi <kristen@linux.intel.com>
2019-01-25 14:13:38 -0800

    fix to install source rpm
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>

--- ci/install_kernel.sh
+++ ci/install_kernel.sh
@@ -2,6 +2,7 @@
 
 DIR="${BASH_SOURCE%/*}"
 RPMDIR="$HOME/rpmbuild/RPMS/x86_64/"
+SRPMDIR="$HOME/rpmbuild/SRPMS/"
 source "$DIR/ccloudvm.bash"
 
 sshcmd=$(getSSH)
@@ -20,8 +21,12 @@ key=$(getHostKey)
 echo key is $key
 
 scp -F /dev/null -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -P $port $kernel $hostip:
-
-runCmdInVM "sudo rpm -i $kernel"
-runCmdInVM "sudo grub2-set-default 0"
-restartVM
 popd
+pushd $SRPMDIR
+kernelsrc=`find . -regextype sed -regex ".*/kernel-[0-9].*"`
+scp -F /dev/null -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -P $port $kernelsrc $hostip:
+popd
+runCmdInVM "sudo rpm -i $kernel --force"
+runCmdInVM "sudo grub2-set-default 0"
+runCmdInVM "sudo rpm -i $kernelsrc --force"
+restartVM
